ff40589df7c828c62e30b61f2df6e6518dcf5987
Konstantin-exe <konstantinmuellner@gmail.com>
2021-03-26 17:26:05 +0100

    added serverconnection, vars not correct

---
+++ pages/api/loginData.js
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// export default (req, res) => {
+//   res.status(200).json({ status: 'Ok' });
+// };
+
+import postgres from 'postgres';
+import camelcaseKeys from 'camelcase-keys';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
+
+setPostgresDefaultsOnHeroku();
+// require('dotenv-safe').config();
+
+function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production') {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.__postgresSqlClient) {
+      globalThis.__postgresSqlClient = postgres();
+    }
+    sql = globalThis.__postgresSqlClient;
+  }
+  return sql;
+}
+const sql = connectOneTimeToDatabase();
+
+export async function getUsers() {
+  const items = await sql`SELECT * FROM shop_items`;
+  return camelcaseKeys(items);
+}
+
+export async function getUserById(id) {
+  const matchingItem = camelcaseKeys(
+    await sql`SELECT * FROM shop_items WHERE id = ${id}`,
+  );
+  return matchingItem;
+}
